afb6dc5caadbdd875c69087135064c6252063352
Sybe2007 <gamersybe19842007@gmail.com>
2021-07-29 02:50:41 +0200

    lol

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	bot: {
+		token: 'ODcwMDk4MDg3NDg4NjYzNTUy.YQHz_Q.R58j49HQ8YngzC5tlOE6cPGV7cs',
+		prefix: '-',
+		clientSecret: 'pJK2vq0erCIhzqIXuun5IJQj438E8LAj',
+		clientID: '870098087488663552'
+	},
+	website: {
+		protocol: 'http://',
+		domain: 'verificationlol.ga',
+		port: 8080,
+		captcha: {
+			sitekey: '6Ld0c8gbAAAAAMgVTNQKRBGiMCBOTglVvJrZW3ik',
+			secretkey: '6Ld0c8gbAAAAAL7lU_VMfEUAx4rQSTXCAQva-cjH'
+		}
+	}
+};
